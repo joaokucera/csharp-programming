658d3575757887d938282db5fb0d85a04aa6f4c1
joaokucera <falleirok@gmail.com>
2014-08-13 20:34:45 -0300

    create project
    create project

---
+++ README.md
@@ -0,0 +1,19 @@
+A C# programming source to support video tutorials
+==================================================
+
+The proposal here is to show C# as an option to develop what you want like games or softwares.
+
+Compatility
+===========
+
+Windows.
+
+License
+=======
+
+Licensed under the MIT License.
+
+Change Log
+==========
+
+0.0.1 First version.
---
+++ source/Programming/Programming/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tipos de Dados
+            int meuInteiro = 20;
+            char meuChar = 'a';
+            string minhaString = "palavra";
+
+            float meuFloat = 20.5f;
+            double meuDouble = 20.5d;
+            decimal meuDecimal = 20.5m;
+
+            bool meuBool = true;
+        }
+    }
+}
